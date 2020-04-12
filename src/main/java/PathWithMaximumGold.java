e10ff86942e94ba8b80fc50a4b66400b1b9bf4bb
Ishan R Tripathi <1605279@kiit.ac.in>
2020-04-13 01:01:18 +0530

    https://leetcode.com/problems/path-with-maximum-gold/

---
+++ src/main/java/PathWithMaximumGold.java
@@ -0,0 +1,51 @@
+//Problem Question Link - https://leetcode.com/problems/path-with-maximum-gold/
+
+import java.io.*;
+import java.util.*;
+
+public class PathWithMaximumGold {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int m= sc.nextInt();
+        int[][] grid= new int[m][n];
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; i<n; j++)
+                grid[i][j]= sc.nextInt();
+        }
+        System.out.println(getMaximumGold(grid));
+    }
+    public int getMaximumGold(int[][] grid) {
+        int res=0;
+        for(int i=0; i<grid.length; i++)
+        {
+            for(int j=0; j<grid[0].length; j++)
+            {
+                res= Math.max(res, dfs(Arrays.copyOf(grid, grid.length), i, j));
+            }
+        }
+        return res;
+    }
+
+    int dfs(int[][] grid, int i, int j)
+    {
+        if(!inBounds(grid, i, j))
+            return 0;
+        int origin=grid[i][j];
+        grid[i][j]=0;
+        int amt=0;
+        amt=Math.max(amt, dfs(grid, i+1, j));
+        amt=Math.max(amt, dfs(grid, i, j+1));
+        amt=Math.max(amt, dfs(grid, i-1, j));
+        amt=Math.max(amt, dfs(grid, i, j-1));
+        grid[i][j]=origin;
+        return amt+origin;
+    }
+    boolean inBounds(int[][] grid, int i, int j)
+    {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[i].length || grid[i][j]==0)
+            return false;
+        return true;
+    }
+}
