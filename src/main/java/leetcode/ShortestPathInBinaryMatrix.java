876c7bdf6cfdae389c6b785d8b134fd58a0086b9
ishanrtripathi <ishanrtripathi786@gmail.com>
2021-02-14 22:24:37 +0530

    Utility modification

--- src/main/java/leetcode/ShortestPathInBinaryMatrix.java
+++ src/main/java/leetcode/ShortestPathInBinaryMatrix.java
@@ -83,29 +83,3 @@ public class ShortestPathInBinaryMatrix {
 
 }
 
-class Pair{
-    int x, y;
-    Pair(int x, int y){
-        this.x= x;
-        this.y= y;
-    }
-
-    @Override
-    public boolean equals(Object o){
-        if(!(o instanceof Pair))
-            return false;
-
-        Pair p= (Pair) o;
-        return this.x==p.x && this.y==p.y;
-    }
-
-    @Override
-    public int hashCode(){
-        return x*13 + y*41;
-    }
-
-    @Override
-    public String toString(){
-        return "Co-ordinates : ("+x+","+y+")\n";
-    }
-}
