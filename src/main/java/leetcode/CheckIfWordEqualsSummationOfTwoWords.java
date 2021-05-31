9507386327756a44bcc4a23dbdd01b15322daf31
IshanRTripathi <ishanrtripathi786@gmail.com>
2021-05-31 08:55:55 +0530

    Added tests, new contest solutions

---
+++ src/main/java/leetcode/CheckIfWordEqualsSummationOfTwoWords.java
@@ -0,0 +1,25 @@
+package main.java.leetcode;
+/*
+* link: https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/
+*/
+
+public class CheckIfWordEqualsSummationOfTwoWords {
+    public static void main(String[] args) {
+        CheckIfWordEqualsSummationOfTwoWords obj= new CheckIfWordEqualsSummationOfTwoWords();
+        System.out.println(obj.isSumEqual("aaa", "a", "aab"));
+    }
+    public boolean isSumEqual(String f, String s, String t) {
+        int ff=0, ss=0, tt=0;
+        for(char c: f.toCharArray()){
+            ff= (ff*10)+(c-'a');
+        }
+        for(char c: s.toCharArray()){
+            ss= (ss*10)+(c-'a');
+        }
+        for(char c: t.toCharArray()){
+            tt= (tt*10)+(c-'a');
+        }
+
+        return (ff+ss == tt);
+    }
+}
