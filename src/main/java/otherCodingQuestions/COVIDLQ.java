e906b696b8c4eff74ae6e65262b543b4f3bf6bac
Ishan R Tripathi <ishanrtripathi786@gmail.com>
2021-07-10 23:05:44 +0530

    Update COVIDLQ.java

--- src/main/java/otherCodingQuestions/COVIDLQ.java
+++ src/main/java/otherCodingQuestions/COVIDLQ.java
@@ -7,7 +7,7 @@ public class COVIDLQ {
     public static void main (String[] args) throws java.lang.Exception
     {
         Scanner sc= new Scanner(System.in);
-        int t= sc.nextInt();
+        int t= sc.nextInt(); 
 
         while(t-->0)
         {
